879fc85024128a57324329e1b3eb62089049cdc8
Nitin Shukla <chilledoutnick@gmail.com>
2026-07-23 15:18:44 +0530

    removed legacy code

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -3,16 +3,8 @@ from fastapi import APIRouter
 from .auth import router as auth_router
 from .health import router as health_router
 from .organizations import router as organizations_router
-from .posts import router as posts_router
-from .rate_limits import router as rate_limits_router
-from .tiers import router as tiers_router
-from .users import router as users_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(health_router)
 router.include_router(auth_router)
 router.include_router(organizations_router)
-router.include_router(users_router)
-router.include_router(posts_router)
-router.include_router(tiers_router)
-router.include_router(rate_limits_router)
